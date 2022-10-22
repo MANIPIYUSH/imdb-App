c9bfb8c5ba0083283292bf90b6a50d2b6343e40b
PIYUSH MANI CHAUBEY <42182675+MANIPIYUSH@users.noreply.github.com>
2022-10-22 18:37:41 +0530

    added geners

--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React,{useState} from 'react'
+import Pagination from './Pagination'
 
 function Favourites() {
+  
+  const [curGenere,setCurGenere] = useState("Action");
+
   return (
-    <div>Favourites components</div>
+    <>
+          <div className='mt-4 px-2 flex justify-center flex-wrap space-x-2 '>
+         <button className={curGenere == "All Generes"?'bg-blue-400 text-lg text-white rounded-xl p-1 px-2 font-bold':'bg-gray-400 hover:bg-blue-400 text-lg text-white rounded-xl p-1 px-2 font-bold'}
+        
+         >All Geners</button>
+         <button className={curGenere == "Action"?'bg-blue-400 text-lg text-white rounded-xl p-1 px-2 font-bold':'bg-gray-400 hover:bg-blue-400 text-lg text-white rounded-xl p-1 px-2 font-bold'}>Action</button>
+         </div>
+         <div className='text-center'>
+          <input type="text" placeholder='Search'className='border border-2 text-center p-1 m-2'/>
+          <input type="number" placeholder='Rows' className='border border-2 text-center p-1 m-2' />
+         </div>
+
+         <div className='mt-4'>
+          <Pagination/>
+         </div>
+         
+    </>
   )
 }
 
