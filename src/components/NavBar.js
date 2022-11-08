8eeba11f6eb74cc64f33124cc607c4f84c977d82
PIYUSH MANI CHAUBEY <42182675+MANIPIYUSH@users.noreply.github.com>
2022-11-08 14:48:06 +0530

    edit favourite

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ function NavBar() {
     <div className = "border flex space-x-8 pl-12 items-center py-4">
    <img className='w-[50px] md:w-[60px]' src={Logo} alt="" />
     <Link to="/" className='text-blue-400 font-bold text-2xl md:text-3xl'>Movies</Link>
-    <Link to='favourites' className='text-blue-400 font-bold text-2xl md:text-3xl'>Favrioutes</Link>
+    <Link to='favourites' className='text-blue-400 font-bold text-2xl md:text-3xl'>Favourites</Link>
     </div>
   )
 }
